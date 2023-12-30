3fc9a082f59a6e1364a4e1e88b1a55da33f5122e
Rafael Pantoja <rafaelbp92@gmail.com>
2023-12-30 11:22:21 -0500

    Adding NLU call

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -3,6 +3,10 @@ import json
 # import related models here
 from .models import CarDealer, DealerReview
 from requests.auth import HTTPBasicAuth
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson.natural_language_understanding_v1 \
+    import Features, SentimentOptions, EntitiesOptions, KeywordsOptions
 
 
 # Create a `get_request` to make HTTP GET requests
@@ -68,13 +72,35 @@ def get_dealer_reviews_from_cf (url, dealerId):
             # dealer_doc = dealer["doc"]
             # Create a CarDealer object with values in `doc` object
             review_obj = DealerReview(dealership=review['dealership'], name=review['name'], review=review['review'], purchase_date=review.get('purchase_date', None),car_make=review.get('car_make', None),car_model=review.get('car_model', None),car_year=review.get('car_year', None),sentiment=None,review_id=review['id'],purchase=review['purchase'])
+            review_obj.sentiment = analyze_review_sentiments(review_obj.review)
             results.append(review_obj)
     return results
 
 # Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
-# def analyze_review_sentiments(text):
+def analyze_review_sentiments(text):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
-
+    try:
+        print(text)
+        url = 'https://api.eu-de.natural-language-understanding.watson.cloud.ibm.com/instances/27a51923-fa50-4640-8da0-4514410fd13e'
+        authenticator = IAMAuthenticator('Jbc6N_Dc8fGHqItUlsoFfPneUH7y3Pb8louc3aXK6ydk')
+        natural_language_understanding = NaturalLanguageUnderstandingV1(
+            version='2022-04-07',
+            authenticator=authenticator
+        )
+        natural_language_understanding.set_service_url(url)
+        response = natural_language_understanding.analyze(
+            text=text,
+            features=Features(
+                entities=EntitiesOptions(emotion=True, sentiment=True, limit=2),
+                keywords=KeywordsOptions(emotion=True, sentiment=True,
+                                    limit=2))).get_result()
+        return response['keywords'][0]['sentiment']['label']
+    except:
+        # If any error occurs
+        print("An error occured while trying to get the NLU response")
+        return 'neutral'
+    
+         
 
 
