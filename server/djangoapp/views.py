d358d782ec00d7ca93fb030e0e4e4f239a02fa04
Rafael Pantoja <rafaelbp92@gmail.com>
2023-12-30 11:39:15 -0500

    Adding dealership table

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -96,6 +96,7 @@ def login_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
+    context = {}
     if request.method == "GET":
         url = "https://eu-de.functions.appdomain.cloud/api/v1/web/5676bf81-6094-4b70-9c74-7e46a430a9bb/dealership-package/get-dealership"
         # Get dealers from the URL
@@ -103,12 +104,14 @@ def get_dealerships(request):
         # Concat all dealer's short name
         dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
         # Return a list of dealer short name
-        return HttpResponse(dealer_names)
+        context['dealership_list'] = dealerships
+        return render(request, 'djangoapp/index.html', context)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
 def get_dealer_details(request, dealer_id):
+    context = {}
     if request.method == "GET":
         url = "https://eu-de.functions.appdomain.cloud/api/v1/web/5676bf81-6094-4b70-9c74-7e46a430a9bb/dealership-package/get-review"
         # Get dealers from the URL
